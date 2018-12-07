5cf8af5797bdb1b45ecf10ed162709133fb8628d
amy null <null@amy.gg>
2018-12-07 11:00:33 -0500

    v2

--- lib/esp_web/controllers/blog_controller.ex
+++ lib/esp_web/controllers/blog_controller.ex
@@ -3,7 +3,7 @@ defmodule ESPWeb.BlogController do
   require Logger
 
   defp get_key(conn) do
-    conn |> get_req_header("authorizaton") |> hd
+    conn |> get_req_header("authorization") |> hd
   end
 
   defp get_user(conn) do
@@ -34,15 +34,47 @@ defmodule ESPWeb.BlogController do
   end
 
   def server_get_one_post(conn, params) do
-    #
+    guild = params["id"]
+    post = params["post"]
+    response = HTTPoison.get!(System.get_env("INTERNAL_API") <> "/data/server/#{guild}/post/#{post}").body
+    conn |> json(response)
   end
 
   def server_edit_one_post(conn, params) do
-    #
+    guild = params["id"]
+    post = params["post"]
+    {state, response} = key_owns_guild conn, guild
+    case state do
+      :ok ->
+        title = params["title"]
+        content = params["content"]
+        {:ok, user_id} = get_user conn
+        account_id = HTTPoison.get!(System.get_env("INTERNAL_API") <> "/data/account/links/discord/" <> user_id).body
+        data = %{
+          "title" => title,
+          "content" => content,
+          "author" => account_id,
+          "guild" => guild,
+          "id" => post
+        }
+        response = HTTPoison.put!(System.get_env("INTERNAL_API") <> "/data/server/#{guild}/post/#{post}", Poison.encode!(data)).body
+        conn |> json(Poison.decode!(response))
+      :error ->
+        conn |> json(%{"error" => response})
+    end
   end
 
   def server_delete_one_post(conn, params) do
-    #
+    guild = params["id"]
+    {state, response} = key_owns_guild conn, guild
+    case state do
+      :ok ->
+        post = params["post"]
+        HTTPoison.delete!(System.get_env("INTERNAL_API") <> "/data/server/#{guild}/post/#{post}")
+        conn |> json(%{})
+      :error ->
+        conn |> json(%{"error" => response})
+    end
   end
 
   def server_post_create(conn, params) do
@@ -50,9 +82,18 @@ defmodule ESPWeb.BlogController do
     {state, response} = key_owns_guild conn, guild
     case state do
       :ok ->
-        # response = HTTPoison.post!(System.get_env("INTERNAL_API") <> "/data/server/#{guild}/post", Poison.encode!(data)).body
-        # conn |> json(response)
-        conn |> json(%{})
+        title = params["title"]
+        content = params["content"]
+        {:ok, user_id} = get_user conn
+        account_id = HTTPoison.get!(System.get_env("INTERNAL_API") <> "/data/account/links/discord/" <> user_id).body
+        data = %{
+          "title" => title,
+          "content" => content,
+          "author" => account_id,
+          "guild" => guild,
+        }
+        response = HTTPoison.post!(System.get_env("INTERNAL_API") <> "/data/server/#{guild}/post", Poison.encode!(data)).body
+        conn |> json(Poison.decode!(response))
       :error ->
         conn |> json(%{"error" => response})
     end
